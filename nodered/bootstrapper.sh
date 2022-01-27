94eda626b5446e1d2fc71dc841dc63cafc7bcb36
Romeo Kienzler <romeokienzler@li-c7445bcc-23db-11b2-a85c-f854e76b2a39.ibm.com>
2022-01-27 17:37:00 +0100

    next version

--- nodered/bootstrapper.sh
+++ nodered/bootstrapper.sh
@@ -1,7 +1,4 @@
-curl -o /data/flows.json https://raw.githubusercontent.com/romeokienzler/rumadi/main/nodered/flows.json
-curl -o /data/flows_cred.json.aes256 https://raw.githubusercontent.com/romeokienzler/rumadi/main/nodered/flows_cred.json.aes256
-openssl enc -d --aes256 -in /data/flows_cred.json.aes256 -out /data/flows_cred.json
+#!/bin/bash
 echo $creds_pass |openssl enc -d --aes256 -in /data/flows_cred.json.aes256 -out /data/flows_cred.json -pass 'stdin'
-npm install node-red-contrib-ewelink
-npm install node-red-dashboard
+npm --no-update-notifier --no-fund start --cache /data/.npm -- --userDir /data
 
